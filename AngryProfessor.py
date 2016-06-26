33d21363a266862eb5d1fb5e890e0091e86e6020
Ken Lee <kzl5010@users.noreply.github.com>
2016-06-25 22:27:04 -0400

    Update AngryProfessor.py

--- AngryProfessor.py
+++ AngryProfessor.py
@@ -1,11 +1,11 @@
 #! /bin/python
 
 t = int(input())
-for i in range(t):
-  a = map(int, input().split())
-  b = map(int, input().split())
-  d = len([e for e in b if e <= 0])
-  if d < a[1]: 
-    print("YES")
-  else: 
-    print("NO")
+for a0 in range(t):
+    n = list(map(int, input().split()))
+    k = map(int, input().split())
+    d = len([e for e in k if e <= 0])
+    if d < n[1]: 
+        print("YES")
+    else: 
+        print("NO")
